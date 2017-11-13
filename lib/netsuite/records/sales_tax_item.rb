9e6546edc87d7e7eff686c9b300ab9f357d7bfc1
Angel Lazarov <avlazarov@gmail.com>
2017-11-13 15:37:10 +0200

    Add a search to sales tax items

--- lib/netsuite/records/sales_tax_item.rb
+++ lib/netsuite/records/sales_tax_item.rb
@@ -8,13 +8,15 @@ module NetSuite
       include Namespaces::ListAcct
 
       actions :get, :get_list, :get_all, :get_select_value, :add, :delete,
-        :update, :upsert
+        :update, :upsert, :search
 
       fields :item_id, :display_name, :description, :rate, :is_inactive,
         :effective_from, :valid_until, :include_children, :eccode,
         :reverse_charge, :service, :exempt, :is_default,
         :exclude_from_tax_reports, :available, :export, :county, :city, :state,
         :zip
+
+      field :subsidiary_list, RecordRefList
 
       record_refs :tax_type, :tax_agency, :tax_account, :purchase_account,
         :sale_account, :parent, :nexus_country
